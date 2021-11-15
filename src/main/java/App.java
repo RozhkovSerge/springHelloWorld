9bfe510625fe2aad45de0b1bd6e83d1ef136a247
SergeRozkov <rozkov_ss@mail.ru>
2021-11-15 14:53:29 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,26 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+
+        System.out.println(bean == bean1);
+        System.out.println(catBean == catBean1);
+
+
+
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    public Cat() {
+        this.message = "Meow!";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
